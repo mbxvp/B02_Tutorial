d70c91d33ae21b14d85ab8e6dc7c93dd875bea12
mbxvp <mbxvp@nottingham.ac.uk>
2018-11-22 12:34:08 +0000

    set up file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tutorial",
+    version="0.0.1",
+    description="Calls you a Bitchface",
+    zip_safe=False,
+    author="Vanisha Patel",
+    author_email="vanisha.patel@nottingham.ac.uk",
+    url="",
+    license="Apache",
+    packages=find_packages(),
+)
